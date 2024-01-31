7c89f55bfe0bd30b6d8ec7314b18b1e6eb6f2811
drchiragmotwani <157987275+drchiragmotwani@users.noreply.github.com>
2024-01-31 13:05:14 -0500

    Demo Video

--- Assets/ObstacleMiddleScript.cs
+++ Assets/ObstacleMiddleScript.cs
@@ -6,12 +6,14 @@ public class ObstacleMiddleScript : MonoBehaviour
 {
     public LogicScript logic;
     public ObstacleSpawnerScript spawner;
+    public CharacterScript character;
 
     // Start is called before the first frame update
     void Start()
     {
         logic = GameObject.Find("LogicManager").GetComponent<LogicScript>();
         spawner = GameObject.Find("ObstacleSpawner").GetComponent<ObstacleSpawnerScript>();
+        character = GameObject.Find("Character").GetComponent<CharacterScript>();
     }
 
     // Update is called once per frame
@@ -22,7 +24,7 @@ public class ObstacleMiddleScript : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.layer == 3)
+        if (collision.gameObject.layer == 3 && character.characterIsAlive)
         {
             logic.AddScore(1);
             spawner.ChangeSpawnRate(logic.playerScore);
